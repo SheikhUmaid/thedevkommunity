203e51483652b91e664696d2db9fe25385a394d9
sheikh umaid <73013686+SheikhUmaid@users.noreply.github.com>
2022-06-17 18:05:59 +0530

    from pathlib import path str error "four"

--- devproject/settings.py
+++ devproject/settings.py
@@ -11,9 +11,9 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-
+from pathlib import Path
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
