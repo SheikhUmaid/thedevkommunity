39fb2d0f2c6738c593a82ce99fd50c1fa17d31a4
sheikh umaid <73013686+SheikhUmaid@users.noreply.github.com>
2022-11-14 23:44:28 +0530

    Update settings.py

--- devproject/settings.py
+++ devproject/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'gj$+v_tkjb^dl$q3(y2ddduj15b@s=t=3egnvbnxk+&z$$q3^e58f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
+
 
 ALLOWED_HOSTS = ['127.0.0.1','thedevkomminuty.herokuapp.com','*','localhost','thedevkommunity.up.railway.app']
 
